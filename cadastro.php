ea4be8a317f5c4c89bc17bb5c34d657ca018c73c
Jeff Lyra <jeff_lyra@outlook.com>
2017-11-27 20:40:11 -0200

    Primeiro Commit

---
+++ cadastro.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+
+<?php 
+
+
+echo "<h1> Mundo, diz </h1>" ; 
+
+?>
+
+
+<body>
+</body>
+</html>
