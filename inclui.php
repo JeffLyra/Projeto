b056ed2e8f200a32978f740c8185768f2ddb37a9
Jeff Lyra <jeff_lyra@outlook.com>
2017-11-27 20:42:51 -0200

    Primeiro teste

---
+++ inclui.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+
+<?php 
+
+
+	echo "<h1> Hello word! </h1>" ; 
+
+
+?>
+
+
+</body>
+</html>
